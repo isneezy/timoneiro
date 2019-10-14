ee94fa53821a1fe5050b3d28f43313e6d8ec56cc
Ivan Vilanculo <vilanculoivan@gmail.com>
2019-10-14 23:04:19 +0200

    update roles table

---
+++ migrations/2019_09_24_163350_create_roles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('display_name');
+            $table->longText('permissions')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->bigInteger('role_id')->unsigned()->after('id')->nullable();
+            $table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn('role_id');
+        });
+
+        Schema::drop('roles');
+
+    }
+}
