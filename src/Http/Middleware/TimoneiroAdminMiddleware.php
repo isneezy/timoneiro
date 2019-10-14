a2fb55c0357ddaa3d629492cfd9ff7a2ebbd4e34
Ivan Vilanculo <vilanculoivan@gmail.com>
2019-10-15 00:22:46 +0200

    Check browse_admin permission

--- src/Http/Middleware/TimoneiroAdminMiddleware.php
+++ src/Http/Middleware/TimoneiroAdminMiddleware.php
@@ -3,19 +3,31 @@
 namespace Isneezy\Timoneiro\Http\Middleware;
 
 use Closure;
+use Isneezy\Timoneiro\Models\User;
 
 class TimoneiroAdminMiddleware
 {
     public function handle($request, Closure $next)
     {
         if (!auth()->guest()) {
+            /** @var User $user */
             $user = auth()->user();
             app()->setLocale($user->locale ?? app()->getLocale());
+            if (!$user->hasPermission('browse_admin')) {
+                if ($this->isInstalledOnRoot()) {
+                    return abort(403);
+                } else {
+                    return redirect('/');
+                }
+            }
 
-            // todo check if the user has permissions to browse admin
-            return true ? $next($request) : redirect('/');
+            return  $next($request);
         }
 
         return redirect()->route('timoneiro.login');
+    }
+
+    public function isInstalledOnRoot() {
+        return route('timoneiro.dashboard') === url('/');
     }
 }
