a0edb1747190109faca8a883d8203072753792b8
Ivan Vilanculo <vilanculoivan@gmail.com>
2019-10-17 10:28:40 +0200

    add dropdown component

--- resources/assets/js/components/index.js
+++ resources/assets/js/components/index.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import FormSelect from './FormSelect'
 import MediaManager from './MediaManager/MediaManager'
 import FileInput from './FileInput'
+import DropDown from './DropDown'
 
 Vue.component('form-select', FormSelect)
 Vue.component('media-manager', MediaManager)
 Vue.component('file-input', FileInput)
+Vue.component('dropdown', DropDown)
