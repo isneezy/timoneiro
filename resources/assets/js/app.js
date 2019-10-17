5256c5b87a01b27a27a9f0ced8812086974731f3
Ivan Vilanculo <vilanculoivan@gmail.com>
2019-10-17 17:56:08 +0200

    show warning message before delete

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,23 +1,32 @@
 import Vue from 'vue'
+import VueCompositionApi from '@vue/composition-api'
+import { onMounted } from '@vue/composition-api'
 import PortalVue from 'portal-vue'
 import axios from 'axios'
 import './components'
+import { initDeleteActions } from './helpers'
 
 window.$ = window.jQuery = require('jquery')
 
 axios.defaults.headers['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 
 Vue.use(PortalVue)
+Vue.use(VueCompositionApi)
 
 $(document).ready(function () {
-    const searchForm = $('#search-form')
-    $('#search-form select').change(() => {
-        searchForm.submit()
-    })
+  const searchForm = $('#search-form')
+  $('#search-form select').change(() => {
+    searchForm.submit()
+  })
 })
 
 const app = new Vue({
-    el: '#app'
+  el: '#app',
+  setup () {
+    onMounted(() => {
+      initDeleteActions()
+    })
+  }
 })
 
 window.$app = app
